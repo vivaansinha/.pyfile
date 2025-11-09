28c6afe125ae4f6df870d969ef1f0fcb4da075eb
vivaansinha <vivaan.sinha.us@gmail.com>
2025-11-08 23:45:12 -0500

    bruh
    watch plssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss 
    https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1

---
+++ google browser terminal.py
@@ -0,0 +1,64 @@
+import webbrowser
+from tkinter import *
+import webbrowser
+def important():
+    x = input("username ")
+    print(f"hi {x}")
+    while True:
+        z = input()
+        if z == "start":
+            aa = input("what link ")
+            webbrowser.open_new_tab(aa)
+window = Tk()
+button = Button(window, text="sign in",command=important)
+button.pack()
+window.geometry("850x666")
+
+
+
+
+window.mainloop()
+
+print('success')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
